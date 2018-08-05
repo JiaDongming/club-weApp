74f58f14ce09450f7c6c8fa4f18757a970143947
JiaDongming <335683442@qq.com>
2018-08-06 07:58:12 +0800

    separate data and show

--- pages/course/course.js
+++ pages/course/course.js
@@ -2,21 +2,38 @@
 var app = getApp()
 Page({
   data: {
-    currentTab: 0
+    currentTab: 0,
+    tab1:'私教预约',
+    tab2:'团体课预约',
+    trainers: [{
+        "img": 'http://img3.imgtn.bdimg.com/it/u=1924672793,3059555888&fm=27&gp=0.jpg',
+        "name": '周瑜',
+        "level": '国家一级运动员',
+        "desc": '在健身俱乐部中一对一进行指导训练的专业健身教练,其具有互...，针对性等',
+        "schedule": '立即预约'
+      },
+      {
+        "img": 'http://img1.imgtn.bdimg.com/it/u=632121556,1788732339&fm=27&gp=0.jpg',
+        "name": '谢敏',
+        "level": '国家一级瑜伽教练',
+        "desc": '在健身俱乐部中一对一进行指导训练的专业健身教练,其具有互...，针对性等',
+        "schedule": '立即预约'
+      }
+    ]
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     // 页面初始化 options为页面跳转所带来的参数
 
   },
   //滑动切换
-  swiperTab: function (e) {
+  swiperTab: function(e) {
     var that = this;
     that.setData({
       currentTab: e.detail.current
     });
   },
   //点击切换
-  clickTab: function (e) {
+  clickTab: function(e) {
     var that = this;
     if (this.data.currentTab === e.target.dataset.current) {
       return false;
