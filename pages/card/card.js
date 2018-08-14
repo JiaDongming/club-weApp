e0797d0c1aaab06d8ae5683fddd1725462e5a9c2
JiaDongming <335683442@qq.com>
2018-08-14 04:05:29 -0700

    20180814

--- pages/card/card.js
+++ pages/card/card.js
@@ -5,62 +5,87 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    imgUrls: [
+'http://himg2.huanqiu.com/attachment2010/2015/0803/10/07/20150803100718671.jpg',
+'http://himg2.huanqiu.com/attachment2010/2016/0901/09/38/20160901093835617.jpg',
+'http://img02.tooopen.com/images/20150619/tooopen_sy_131361934346.jpg',
+'http://img02.tooopen.com/Downs/images/2010/5/23/sy_20100523125438553037.jpg'
+    ],
+    articles: [{
+        title: "重庆两江交汇“泾渭分明” 一清一浑变“鸳鸯锅”",
+        imgUrl: 'http://imgmini.eastday.com/push/20180814/930x620_1534204049527897.jpg',
+      link:'../../article/article01/article01',
+        content: '2018年8月13日，重庆朝天门港的长江、嘉陵江交汇处出现“阴阳水”自然奇观，江水变得一清一浑，泾渭分明，好似重庆火锅中的鸳鸯锅，十分壮观。现场看到，黄色江水的是长江，绿色江水的是嘉陵江，两种颜色的江水在朝天门水域汇合交杂，形成“阴阳水”奇观。这种现象十分罕见，一般出现在每年夏季，且长江当日受上游川、陕地区强降雨影响，而嘉陵江上游未出现降雨的情况下才能形成。 图片来源：东方IC 版权作品 请勿转载'
+      },
+      {
+        title: "特朗普签署7160亿美元国防授权法案 军费增加2.6%",
+        imgUrl: 'https://t1.huanqiu.cn/a3c6600f74379db62cbde78494230e34.jpg',
+        link: '../../article/article02/article02',
+        content: '当地时间2018年8月13日，美国纽约德拉姆堡军事基地，美国总统特朗普签署总额达7160亿美元的2019年度国防授权法案，法案正式生效。由此，美军军费将增加2.6%，为9年来最大增幅。'
+      },
+      {
+        title: "中国国家游泳队举行公开训练课",
+        imgUrl: 'https://res.youth.cn/article_201808_14_14g_5b720ea467d9b.jpg',
+        link: '../../article/article03/article03',
+        content: '8月13日，中国国家游泳队在北京举行雅加达亚运会前的最后一场公开训练课。图为部分队员训练后合影留念。 中新社记者 韩海丹 摄'
+      },
+    ]
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
